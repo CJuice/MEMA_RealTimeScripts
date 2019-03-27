b90374033791d91b8f2d1539480123ea682afc7e
CJuice <CJuice@users.noreply.github.com>
2019-03-27 08:11:09 -0400

    documentation and cleanup

--- task_HospitalStatus/doit_HospitalStatus.py
+++ task_HospitalStatus/doit_HospitalStatus.py
@@ -1,10 +1,19 @@
 """
-REDESIGN
+This is a procedural script for populating MEMA database with hospital status data.
+
+This process accesses three “CHATS Region County/Hospital Alert Tracking System” html pages containing tables
+on the status of hospitals in a few different areas of interest to emergency management. The process pulls the
+hospitals table into a python pandas dataframe and processes the data into SQL statements. The SQL statements
+are used to insert the table data into a SQL table tracking the most data as of the last process. The SQL table
+is accessed by the OSPREY Dashboard and influences the results in the hospitals row.
+Redesigned from the original CGIS version when MEMA server environments were being migrated to new versions.
+Author: CJuice, 20190327
 
 """
-# TODO: Documentation
+
 
 def main():
+
     # IMPORTS
     from datetime import datetime
     import configparser
@@ -17,10 +26,8 @@ def main():
     # VARIABLES
     _root_file_path = os.path.dirname(__file__)
     config_file_path = r"doit_config_HospitalStatus.cfg"
-    config_section_value_of_interest = "url"
     database_cfg_section_name = "DATABASE"
     database_connection_string = "DSN={database_name};UID={database_user};PWD={database_password}"
-    hospitals_cfg_section_name = "HOSPITALSTATUS"
     html_id_hospital_table = "tblHospitals"
     realtime_hospitalstatus_headers = (
     "Linkname", "Status", "Yellow", "Red", "Mini", "ReRoute", "t_bypass", "DataGenerated")
@@ -29,35 +36,49 @@ def main():
     sql_statements_list = []
     sql_values_statement = """({values})"""
     sql_values_string_template = """'{hospital}', '{status_level_value}', '{red_alert}','{yellow_alert}', '{mini_disaster}', '{reroute}', '{trauma_bypass}', '{created_date_string}'"""
+    urls_list = ["https://www.miemssalert.com/chats/Default.aspx?hdRegion=3",
+                 "https://www.miemssalert.com/chats/Default.aspx?hdRegion=124",
+                 "https://www.miemssalert.com/chats/Default.aspx?hdRegion=5"]
 
     # ASSERT STATEMENTS
     assert os.path.exists(config_file_path)
 
     # FUNCTIONS
-    def create_database_connection_string(db_name, db_user, db_password):
+    def create_database_connection_string(db_name: str, db_user: str, db_password: str) -> str:
         """
-        Create the connection string for accessing database and assign to internal attribute.
-        :return: none
+        Create the connection string for accessing database and return.
+        :return: string, sql connection
         """
         return database_connection_string.format(database_name=db_name,
                                                  database_user=db_user,
                                                  database_password=db_password)
 
-    def create_date_time_value():
+    def create_date_time_value() -> str:
+        """
+        Create a formatted date and time value as string
+        :return: string date & time
+        """
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     def determine_status_level(html_row_series: pd.Series):
-        """Evaluate presence of data in html table and return string based on business logic tree.
-        This is reproduced functionality from interpretation of single line statement in old code
-        that determined 'Status' dictionary keys corresponding value
-        OLD: "red" if row[4] is not '' else "yellow" if row[3] is not '' or row[6] is not ''
-                else "t_bypass" if row[7] is not '' else "mini" if row[5] is not '' else "normal"
-        'row' was a record from an html table, with two values appended at the beginning. The old process was
-        basically looking for a value other than null/empty and there is a hierarchy of importance if values
-        are simultaneously present. The old way created a list ('row') that started with created date and current date,
-        then the row contents from html table. Redesign subtracts two from old index positions since the two date
-        values are no longer a factor.
         """
+        Evaluate presence of data in html table and return string based on business logic tree.
+
+        This is reproduced functionality from interpretation of single line statement in old code
+        that determined the 'Status' value in database table
+        OLD PYTHON STATEMENT:
+            "red" if row[4] is not '' else "yellow" if row[3] is not '' or row[6] is not ''
+            else "t_bypass" if row[7] is not '' else "mini" if row[5] is not '' else "normal"
+        'row' was a record from an html table, with two values appended at the beginning. The old process was
+        basically looking for a value other than null/empty, and there is a hierarchy of importance if values
+        are simultaneously present. The old way created a list called 'row' that started with created date and
+        current date, then the row contents from html table. Redesign subtracts two from old index positions
+        since the two date values are no longer a factor.
+
+        :param html_row_series: pandas series containing data from a row of html table
+        :return:
+        """
+        # Get the values in the table or a default of numpy NaN
         yellow_alert_ser_val = html_row_series.get(key="Yellow Alert", default=np.NaN)
         red_alert_ser_val = html_row_series.get(key="Red Alert", default=np.NaN)
         mini_disaster_ser_val = html_row_series.get(key="Mini Disaster", default=np.NaN)
@@ -83,34 +104,48 @@ def main():
                     else:
                         return "normal"
 
-    def setup_config(cfg_file):
+    def setup_config(cfg_file: str) -> configparser.ConfigParser:
+        """
+        Instantiate the parser for accessing a config file.
+        :param cfg_file: config file to access
+        :return:
+        """
         cfg_parser = configparser.ConfigParser()
         cfg_parser.read(filenames=cfg_file)
         return cfg_parser
 
-    def write_response_to_html(response_content, filename):
-        with open(filename, "w") as handler:
-            handler.write(response_content)
-        return
+    # def write_response_to_html(response_content: str, filename: str):
+    #     """
+    #     Write content to file.
+    #     This was for writing the html pages to file. Unsure of reason this was done in old process. Recreated during
+    #     development but decided to comment this out for production as the output html files do not appear to be
+    #     consumed by any process or examined at any point by a person. The process was deemed wasteful and unnecessary
+    #     and was dropped. The code remains in case a need is discovered.
+    #
+    #     :param response_content: Intended to be content of request response, html table in this process
+    #     :param filename: name of the file to be written
+    #     :return: none
+    #     """
+    #     with open(filename, "w") as handler:
+    #         handler.write(response_content)
+    #     return
+    pass  # only here to get above commented out function to be collapsible and separate from FUNCTIONALITY
 
     # FUNCTIONALITY
-    # get current datetime stamp
+    # need a current datetime stamp for database entry
     current_date_time = str(create_date_time_value())
     print(f"Process Date & Time: {current_date_time}")
 
     # need parser to access credentials
     parser = setup_config(config_file_path)
 
-    # read parser file urls_csv_list, split on commas to get list of three urls
-    urls_csv_list = parser[hospitals_cfg_section_name][config_section_value_of_interest].split(",")
-
     # need the sql table headers as comma separated string values for use in the DELETE & INSERT statement
     headers_joined = ",".join([f"{val}" for val in realtime_hospitalstatus_headers])
     sql_delete_insert_string = sql_delete_insert_template.format(realtime_hospstat_tbl=realtime_hospstat_tbl,
                                                                  headers_joined=headers_joined)
 
-    # for each url in the list need to get data, parse data, process data
-    for url_index, url_string in enumerate(urls_csv_list):
+    # need to get data, parse data, process data for each url in the list
+    for url_index, url_string in enumerate(urls_list):
         print(f"Making request to {url_string}")
         output_filename_path = f"{_root_file_path}/data/HospitalStatus_{url_index}.html"
 
@@ -130,15 +165,15 @@ def main():
         # else:
         # 	print(f"HTML file written: {output_filename_path}")
 
-        # Now using pandas to get html table.
+        # Need the html table in a readable format for use. Pandas dataframe is cheap and easy.
         html_table_dfs_list = pd.read_html(io=response.text, header=0, attrs={"id": html_id_hospital_table})
         html_table_df = html_table_dfs_list[0]  # html id's are unique so should only be one item in list
-        print(html_table_df.info())
+        # print(html_table_df.info())
 
-        # This iteration will provide a row index value and the row data as a dictionary.
+        # Need an iteration to provide rows from the dataframe.
         row_generator = html_table_df.iterrows()
         for row_index, row_series in row_generator:
-            status_level_value = determine_status_level(row_series)
+            status_level_value = determine_status_level(html_row_series=row_series)
             hospital, yellow_alert, red_alert, mini_disaster, reroute, trauma_bypass, *rest = row_series
             values = sql_values_string_template.format(hospital=hospital,
                                                        status_level_value=status_level_value,
