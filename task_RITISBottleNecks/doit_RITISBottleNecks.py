11151778421d738bc9349014dfedd202fa8bf8c4
CJuice <CJuice@users.noreply.github.com>
2019-05-10 15:18:54 -0400

    database transaction indented too far

--- task_RITISBottleNecks/doit_RITISBottleNecks.py
+++ task_RITISBottleNecks/doit_RITISBottleNecks.py
@@ -172,7 +172,7 @@ def main():
     try:
         if not TESTING:
             response = requests.post(url=mema_request_url, data=mema_data, headers=mema_request_header)
-            print(response.status_code)
+            # print(response.status_code)
     except Exception as e:
         print(f"Exception during request for page {mema_request_url}. {e}")
         print(f"Response status code: {response.status_code}")
@@ -264,63 +264,63 @@ def main():
         values_string = sql_values_statement.format(values=values)
         sql_values_statements_list.append(values_string)
 
-        # Database Transactions
-        print(f"\nDatabase operations initiated. Time elapsed {time_elapsed(start=start)}")
-        database_name = config_parser[database_cfg_section_name]["NAME"]
-        database_password = config_parser[database_cfg_section_name]["PASSWORD"]
-        database_user = config_parser[database_cfg_section_name]["USER"]
-        full_connection_string = create_database_connection_string(db_name=database_name,
-                                                                   db_user=database_user,
-                                                                   db_password=database_password)
-        database_table_name = realtime_ritisbottlenecks_tbl.format(database_name=database_name)
+    # Database Transactions
+    print(f"\nDatabase operations initiated. Time elapsed {time_elapsed(start=start)}")
+    database_name = config_parser[database_cfg_section_name]["NAME"]
+    database_password = config_parser[database_cfg_section_name]["PASSWORD"]
+    database_user = config_parser[database_cfg_section_name]["USER"]
+    full_connection_string = create_database_connection_string(db_name=database_name,
+                                                               db_user=database_user,
+                                                               db_password=database_password)
+    database_table_name = realtime_ritisbottlenecks_tbl.format(database_name=database_name)
 
-        # need the sql table headers as comma separated string values for use in the DELETE & INSERT statement
-        headers_joined = ",".join([f"{val}" for val in ritis_bottlenecks_headers])
-        sql_delete_string = sql_delete_template.format(table=database_table_name)
-        sql_insert_string = sql_insert_template.format(
-            table=database_table_name,
-            headers_joined=headers_joined)
+    # need the sql table headers as comma separated string values for use in the DELETE & INSERT statement
+    headers_joined = ",".join([f"{val}" for val in ritis_bottlenecks_headers])
+    sql_delete_string = sql_delete_template.format(table=database_table_name)
+    sql_insert_string = sql_insert_template.format(
+        table=database_table_name,
+        headers_joined=headers_joined)
 
-        # Need the insert statement generator to be ready for database insertion rounds
-        sql_insert_gen = sql_insert_generator(sql_values_list=sql_values_statements_list,
-                                              step_increment=sql_insertion_step_increment,
-                                              sql_insert_string=sql_insert_string)
+    # Need the insert statement generator to be ready for database insertion rounds
+    sql_insert_gen = sql_insert_generator(sql_values_list=sql_values_statements_list,
+                                          step_increment=sql_insertion_step_increment,
+                                          sql_insert_string=sql_insert_string)
 
-        # Build the sql for updating the task tracker table for this process.
-        sql_task_tracker_update = f"UPDATE RealTime_TaskTracking SET lastRun = '{start_date_time}', DataGenerated = (SELECT max(DataGenerated) from {database_table_name}) WHERE taskName = '{task_name}'"
+    # Build the sql for updating the task tracker table for this process.
+    sql_task_tracker_update = f"UPDATE RealTime_TaskTracking SET lastRun = '{start_date_time}', DataGenerated = (SELECT max(DataGenerated) from {database_table_name}) WHERE taskName = '{task_name}'"
 
-        with pyodbc.connect(full_connection_string) as connection:
-            cursor = connection.cursor()
+    with pyodbc.connect(full_connection_string) as connection:
+        cursor = connection.cursor()
 
-            # Due to 1000 record insert limit, delete records first and then do insertion rounds for 2400+ gauges
+        # Due to 1000 record insert limit, delete records first and then do insertion rounds for 2400+ gauges
+        try:
+            cursor.execute(sql_delete_string)
+        except Exception as e:
+            print(f"Error deleting records from {database_table_name}. {e}")
+            exit()
+        else:
+            print(f"Delete statement executed. Time elapsed {time_elapsed(start=start)}")
+
+        # Need insert statement in rounds of 1000 records or less to avoid sql limit
+        insert_round_count = 1
+        for batch in sql_insert_gen:
             try:
-                cursor.execute(sql_delete_string)
-            except Exception as e:
-                print(f"Error deleting records from {database_table_name}. {e}")
-                exit()
-            else:
-                print(f"Delete statement executed. Time elapsed {time_elapsed(start=start)}")
-
-            # Need insert statement in rounds of 1000 records or less to avoid sql limit
-            insert_round_count = 1
-            for batch in sql_insert_gen:
-                try:
-                    cursor.execute(batch)
-                except pyodbc.DataError:
-                    print(f"A value in the sql exceeds the field length allowed in database table: {batch}")
-                else:
-                    print(f"Executing insert batch {insert_round_count}. Time elapsed {time_elapsed(start=start)}")
-                    insert_round_count += 1
-
-            # Need to update the task tracker table to record last run time
-            try:
-                cursor.execute(sql_task_tracker_update)
+                cursor.execute(batch)
             except pyodbc.DataError:
-                print(
-                    f"A value in the sql exceeds the field length allowed in database table: {sql_task_tracker_update}")
+                print(f"A value in the sql exceeds the field length allowed in database table: {batch}")
+            else:
+                print(f"Executing insert batch {insert_round_count}. Time elapsed {time_elapsed(start=start)}")
+                insert_round_count += 1
 
-            connection.commit()
-            print(f"Commit successful. Time elapsed {time_elapsed(start=start)}")
+        # Need to update the task tracker table to record last run time
+        try:
+            cursor.execute(sql_task_tracker_update)
+        except pyodbc.DataError:
+            print(
+                f"A value in the sql exceeds the field length allowed in database table: {sql_task_tracker_update}")
+
+        connection.commit()
+        print(f"Commit successful. Time elapsed {time_elapsed(start=start)}")
 
     print("\nProcess completed.")
     print(f"Time elapsed {time_elapsed(start=start)}")
