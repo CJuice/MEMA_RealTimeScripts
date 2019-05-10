69121c532c760209a5a484f8d36e3888891ee734
CJuice <CJuice@users.noreply.github.com>
2019-05-10 15:10:48 -0400

    Update doit_RITISBottleNecks.py

--- task_RITISBottleNecks/doit_RITISBottleNecks.py
+++ task_RITISBottleNecks/doit_RITISBottleNecks.py
@@ -23,17 +23,22 @@ def main():
     _root_file_path = os.path.dirname(__file__)
     config_file = r"doit_config_RITISBottlenecks.cfg"
     config_file_path = os.path.join(_root_file_path, config_file)
-    current_year = datetime.now().year
+    # current_year = datetime.now().year
     database_connection_string = "DSN={database_name};UID={database_user};PWD={database_password}"
     date_time_format = "%Y-%m-%d %H:%M:%S"
     mema_cfg_section_name = "MEMA_VALUES"
-    realtime_webeocshelters_tbl = "[{database_name}].[dbo].[RealTime_RITISBottlenecks]"  # TODO: Check
+    realtime_ritisbottlenecks_tbl = "[{database_name}].[dbo].[RealTime_RITISBottleNecks]"
     feature_objects_list = []
-    sql_delete_insert_template = """DELETE FROM {table}; INSERT INTO {table} ({headers_joined}) VALUES """
+    ritis_bottlenecks_headers = ("ID", "starttime", "closedtime", "length", "description", "city", "zipcode",
+                                 "stateID", "countyID", "geometry", "DataGenerated")
+    # sql_delete_insert_template = """DELETE FROM {table}; INSERT INTO {table} ({headers_joined}) VALUES """
+    sql_delete_template = """DELETE FROM {table};"""
+    sql_insert_template = """INSERT INTO {table} ({headers_joined}) VALUES """
+    sql_insertion_step_increment = 1000
     sql_values_statement = """({values})"""
     sql_values_statements_list = []
     sql_values_string_template = """'{id}', '{start_time}', '{closed_time}', {length}, '{description}', '{city}', '{zip_code}', {state_id}, '{county_id}', {geometry}, '{data_gen}'"""
-    task_name = "RITISBottlenecks"  # TODO: Check
+    task_name = "RITISBottleNecks"
 
     TESTING = False  # OPTION
 
@@ -54,6 +59,15 @@ def main():
         :return:
         """
         return value.replace("'", "''")
+
+    def create_database_connection_string(db_name: str, db_user: str, db_password: str) -> str:
+        """
+        Create the connection string for accessing database and return.
+        :return: string, sql connection
+        """
+        return database_connection_string.format(database_name=db_name,
+                                                 database_user=db_user,
+                                                 database_password=db_password)
 
     def create_date_time_value_for_db() -> str:
         """
@@ -93,6 +107,22 @@ def main():
         cfg_parser = configparser.ConfigParser()
         cfg_parser.read(filenames=cfg_file)
         return cfg_parser
+
+    def sql_insert_generator(sql_values_list, step_increment, sql_insert_string):
+        """
+        Generator for yielding batches of sql values for insertion
+        Purpose is to work with the 1000 record limit of SQL insertion.
+        :param sql_values_list: list of prebuilt record values ready for sql insertion
+        :param step_increment: the record count increment for insertion batches
+        :param sql_insert_string: sql statement string for use with values
+        :return: yield a string for use in insertion
+        """
+        for i in range(0, len(sql_values_list), step_increment):
+            values_in_range = sql_values_list[i: i + step_increment]
+
+            # Build the entire SQL statement to be executed
+            output = sql_insert_string + ",".join(values_in_range)
+            yield output
 
     def time_elapsed(start=datetime.now()):
         """
@@ -234,8 +264,66 @@ def main():
         values_string = sql_values_statement.format(values=values)
         sql_values_statements_list.append(values_string)
 
-        
-    return
+        # Database Transactions
+        print(f"\nDatabase operations initiated. Time elapsed {time_elapsed(start=start)}")
+        database_name = config_parser[database_cfg_section_name]["NAME"]
+        database_password = config_parser[database_cfg_section_name]["PASSWORD"]
+        database_user = config_parser[database_cfg_section_name]["USER"]
+        full_connection_string = create_database_connection_string(db_name=database_name,
+                                                                   db_user=database_user,
+                                                                   db_password=database_password)
+        database_table_name = realtime_ritisbottlenecks_tbl.format(database_name=database_name)
+
+        # need the sql table headers as comma separated string values for use in the DELETE & INSERT statement
+        headers_joined = ",".join([f"{val}" for val in ritis_bottlenecks_headers])
+        sql_delete_string = sql_delete_template.format(table=database_table_name)
+        sql_insert_string = sql_insert_template.format(
+            table=database_table_name,
+            headers_joined=headers_joined)
+
+        # Need the insert statement generator to be ready for database insertion rounds
+        sql_insert_gen = sql_insert_generator(sql_values_list=sql_values_statements_list,
+                                              step_increment=sql_insertion_step_increment,
+                                              sql_insert_string=sql_insert_string)
+
+        # Build the sql for updating the task tracker table for this process.
+        sql_task_tracker_update = f"UPDATE RealTime_TaskTracking SET lastRun = '{start_date_time}', DataGenerated = (SELECT max(DataGenerated) from {database_table_name}) WHERE taskName = '{task_name}'"
+
+        with pyodbc.connect(full_connection_string) as connection:
+            cursor = connection.cursor()
+
+            # Due to 1000 record insert limit, delete records first and then do insertion rounds for 2400+ gauges
+            try:
+                cursor.execute(sql_delete_string)
+            except Exception as e:
+                print(f"Error deleting records from {database_table_name}. {e}")
+                exit()
+            else:
+                print(f"Delete statement executed. Time elapsed {time_elapsed(start=start)}")
+
+            # Need insert statement in rounds of 1000 records or less to avoid sql limit
+            insert_round_count = 1
+            for batch in sql_insert_gen:
+                try:
+                    cursor.execute(batch)
+                except pyodbc.DataError:
+                    print(f"A value in the sql exceeds the field length allowed in database table: {batch}")
+                else:
+                    print(f"Executing insert batch {insert_round_count}. Time elapsed {time_elapsed(start=start)}")
+                    insert_round_count += 1
+
+            # Need to update the task tracker table to record last run time
+            try:
+                cursor.execute(sql_task_tracker_update)
+            except pyodbc.DataError:
+                print(
+                    f"A value in the sql exceeds the field length allowed in database table: {sql_task_tracker_update}")
+
+            connection.commit()
+            print(f"Commit successful. Time elapsed {time_elapsed(start=start)}")
+
+    print("\nProcess completed.")
+    print(f"Time elapsed {time_elapsed(start=start)}")
 
 
 if __name__ == "__main__":
